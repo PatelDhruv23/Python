d8115cb0f119f7f6c510adc422787f14e51bd465
PatelDhruv23 <dhrruv141@gmail.com>
2026-01-26 17:49:52 +0530

    updated Python  work

---
+++ DAY-12/pattern4.py
@@ -0,0 +1,13 @@
+
+#! *        
+#! * *      
+#! * * *    
+#! * * * *  
+#! * * * * *
+
+n= int(input("Enter Number: "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i >= j:
+            print("*", end=" ")
+    print()
